904d77f366605d360908afd5f1921ffa1e7142ac
Vinayak Fialok <vinayak.fialok@outlook.com>
2022-10-10 10:46:55 +1100

    Added heatmap grid and hover effect

---
+++ src/components/HeatmapContainer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import HeatmapCell from './HeatmapCell'
+
+let cells = [{}]
+
+const HeatmapContainer = () => {
+  const generateCells = (numCells) => {
+    return Array(numCells).fill(1)
+      .map((ie, i) => cells.push({key: i, cell: <HeatmapCell value={Math.floor(Math.random()*1000)}/>}));
+  }
+
+  generateCells(365);  
+
+  return (
+    <div className="grid grid-cols-26 gap-10 p-4 max-w-full" 
+         style={{
+            backgroundColor: `rgba(10, 10, 20)`
+          }}>
+      {cells.map(x => x.cell)}
+    </div>
+  )
+}
+
+export default HeatmapContainer
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        '16': 'repeat(16, minmax(0, 1fr))',
+        '20': 'repeat(20, minmax(0, 1fr))',
+        '22': 'repeat(22, minmax(0, 1fr))',
+        '24': 'repeat(24, minmax(0, 1fr))',
+        '26': 'repeat(26, minmax(0, 1fr))'
+      },
+      colors: {
+        'theme-pinkblue': {
+          0: 'rgba(0, 100, 200)',
+          100: 'rgba(25, 100, 200)',
+          200: 'rgba(51, 100, 200)',
+          300: 'rgba(77, 100, 200)',
+          400: 'rgba(102, 100, 200)',
+          500: 'rgba(127, 100, 200)',
+          600: 'rgba(153, 100, 200)',
+          700: 'rgba(178, 100, 200)',
+          800: 'rgba(204, 100, 200)',
+          900: 'rgba(230, 100, 200)',
+          1000: 'rgba(255, 100, 200)',
+        }
+      },
+    },
+  },
+  plugins: [],
+}
