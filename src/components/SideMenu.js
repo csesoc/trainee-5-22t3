71d780be617225ae31ac0d1873d401dc26cb2d1f
Agnes Clemente <agnesclemente@192-168-1-101.tpgi.com.au>
2022-10-17 21:09:51 +1100

    added habit component

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,8 +1,17 @@
+import Habit from "./Habit"
+
 const SideMenu = () => {
 
     return (
         <div className='bg-[#201D26] inline-block w-1/4 rounded-3xl text-[#B7B1C7] p-[10px]'>
-            Side Menu
+            Do
+            <Habit habit="make bed"/>
+            <Habit habit="clean room"/>
+            <Habit habit="study"/>
+            Don't Do
+            <Habit habit="tiktok"/>
+            <Habit habit="sleep late"/>
+            <Habit habit="nap"/>
         </div>
     )
 }
