910d60bd99ad26619910435c312118b175e3fd54
VinayakFia <96902642+VinayakFia@users.noreply.github.com>
2022-10-30 22:22:43 +1100

    Vinnie/heatmap reactive (#6)
    * Added heatmap reactivity to width if given height and width inputs are correct
    
    * Minor formatting changes
    
    * changes
    
    * Made data be fetched in App.js, Made heatmap styling work with variables to make the codebase alot simpler, moved styling to work with string interpolation, changed cells array to store variables instead of HeatMap cell components
    
    * Changed rounding since the size is now guaranteed at a minimum
    
    * Changed heatmap cell return to be simpler
    
    * Fixed infinte loop useeffect
    
    * Loading animation kind of
    
    * Fixed issue with inset styling

--- src/App.js
+++ src/App.js
@@ -1,32 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
-import HeatmapContainer from './components/HeatmapContainer';
-import { useRef } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import HeatmapContainer from "./components/HeatmapContainer";
+import {
+  useContext,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
+import { heatmapCellStates } from "./components/consts";
 
-Date.prototype.addDays = function(days) {
+Date.prototype.addDays = function (days) {
   var date = new Date(this.valueOf());
   date.setDate(date.getDate() + days);
   return date;
-}
-const START_DATE = new Date('1/1/2022');
-const data = Array(200).fill(1)
+};
+
+const START_DATE = new Date("1/1/2022");
+const dummyData = Array(200)
+  .fill(1)
   .map((ie, i) => {
     let date = START_DATE;
     date = date.addDays(i);
     return {
       date: date,
       index: i,
-      note: 'placeholder',
-      wins: Array(10).fill(1).map(x => Math.round(Math.random()*10)),
-      losses: Array(10).fill(1).map(x => Math.round(Math.random()*10)),
-      value: Math.round(Math.random()*1000)
+      note: "placeholder",
+      wins: Array(10)
+        .fill(1)
+        .map((x) => Math.round(Math.random() * 10)),
+      losses: Array(10)
+        .fill(1)
+        .map((x) => Math.round(Math.random() * 10)),
+      value: Math.round(Math.random() * 1000),
     };
   });
 
-function App() {  
+const fetchData = () => {
+  return dummyData;
+};
+
+function App() {
+  const [data, setData] = useState(heatmapCellStates.loading);
+  const ref = useRef(null);
+
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  const setHeatmapDimensions = () => {
+    setWidth(ref.current.offsetWidth);
+    setHeight(ref.current.offsetHeight);
+  };
+
+  useLayoutEffect(() => {
+    setHeatmapDimensions();
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", setHeatmapDimensions);
+    setTimeout(() => {
+      const res = fetchData();
+      setData(res);
+    }, 4000); // wait so 2 seconds here
+    return () => {
+      window.removeEventListener("resize", setHeatmapDimensions);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <HeatmapContainer data={data} highlightProperties={Array(1).fill(1)} isPropertiesHighlighted={true}/>
+      <div ref={ref} className="h-full">
+        <HeatmapContainer
+          data={data}
+          highlightProperties={Array(1).fill(1)}
+          isPropertiesHighlighted={false}
+          height={height}
+          width={width}
+        />
+      </div>
     </div>
   );
 }
--- src/components/HeatmapContainer.js
+++ src/components/HeatmapContainer.js
@@ -1,31 +1,80 @@
-import React, { useEffect, useState } from 'react'
-import { heatmapCellStates } from './consts';
-import HeatmapCell from './HeatmapCell'
+import React, { useEffect, useState } from "react";
+import {
+  HeatmapCellSize,
+  heatmapCellStates,
+  HeatmapContainerGap,
+  HeatmapContainerGapCSS,
+  HeatmapContainerMaxCells,
+} from "./consts";
+import HeatmapCell from "./HeatmapCell";
 
-const HeatmapContainer = ({data, highlightProperties, isPropertiesHighlighted}) => {
+const HeatmapContainer = ({
+  data,
+  highlightProperties,
+  isPropertiesHighlighted,
+  height,
+  width,
+}) => {
+  // Handle null input first (i.e. data is stil loading)
   const cells = [];
-  const generateCells = () => {
-    Array(data.length).fill(1)
-      .map((ie, i) => cells.push({info: data[i], cell: <HeatmapCell value={data[i].value} state={heatmapCellStates.default}/>})
-    );
+
+  data === null
+    ? Array(HeatmapContainerMaxCells)
+        .fill(1)
+        .map((ie, i) =>
+          cells.push({
+            key: Math.random() * 1000000,
+            value: 0,
+            state: heatmapCellStates.loading,
+            info: null,
+          })
+        )
+    : Array(data.length)
+        .fill(1)
+        .map((ie, i) =>
+          cells.push({
+            key: Math.random() * 1000000,
+            value: data[i].value,
+            state: heatmapCellStates.default,
+            info: data[i],
+          })
+        );
+
+  if (isPropertiesHighlighted && data !== heatmapCellStates.loading) {
+    for (const highlightProperty of highlightProperties) {
+      cells.map((x) =>
+        cells.map((x, i) => {
+          !x.info.wins.includes(highlightProperty) &&
+          !x.info.losses.includes(highlightProperty)
+            ? (x.state = heatmapCellStates.isDimmed)
+            : (x.state = heatmapCellStates.isHighlighted);
+        })
+      );
+    }
   }
 
-  generateCells();
-  if (isPropertiesHighlighted) 
-    for (const highlightProperty of highlightProperties) {
-      cells.map((x) => cells.map((x, i) => {
-        if (!x.info.wins.includes(highlightProperty) && !x.info.losses.includes(highlightProperty)) x.cell =  <HeatmapCell key={Math.random()*1000000} value={cells[i].info.value} state={heatmapCellStates.isDimmed} info={x.info}/>
-        else x.cell = <HeatmapCell key={Math.random()*1000000} value={cells[i].info.value} state={heatmapCellStates.isHighlighted} info={x.info}/>
-      }))
-    }
+  const numCols = Math.floor(
+    (width + HeatmapContainerGap) / (HeatmapCellSize + HeatmapContainerGap)
+  );
+  const numRows = Math.floor(
+    (height + HeatmapContainerGap) / (HeatmapCellSize + HeatmapContainerGap)
+  );
+  const cellsToRender = cells.slice(-numRows * numCols);
 
-  return(
-    <div className="grid grid-cols-24 xl:grid-cols-24 lg:grid-cols-22 2xl:grid-cols-26 gap-8 p-4 max-w-full" style={{
-            backgroundColor: `rgba(10, 10, 20)`
-        }}>
-      {cells.map(x => x.cell)}
+  return (
+    <div
+      className={`grid ${
+        "grid-cols-" + numCols
+      } ${HeatmapContainerGapCSS} p-4 max-w-full`}
+      style={{
+        backgroundColor: `rgba(10, 10, 20)`,
+      }}
+    >
+      {cellsToRender.map(({ key, value, state, info }) => (
+        <HeatmapCell key={key} value={value} state={state} info={info} />
+      ))}
     </div>
   );
-}
-  
+};
+
 export default HeatmapContainer;
